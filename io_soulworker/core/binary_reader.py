cd99a71c9fb1c61f5aef109a625496317a4b19ab
sawich <sawich@angeldev.org>
2023-10-20 04:38:33 +0300

    Add missing chunk id

--- io_soulworker/core/binary_reader.py
+++ io_soulworker/core/binary_reader.py
@@ -83,7 +83,7 @@ class BinaryReader(BufferedReader):
     def read_cid(self) -> VisChunkId:
         """ Chunk ID """
 
-        return VisChunkId(self.read_uint32())
+        return VisChunkId(self.read_int32())
 
     def read_float(self) -> float:
         return float(unpack("<f", self.read(4))[0])
